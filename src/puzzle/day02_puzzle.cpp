44458006bb03779823e222a12a23f4b8c3ec866d
Tom LeGodais <tomlegodais@gmail.com>
2023-12-10 03:38:32 -0500

    start on day02_puzzle.cpp

--- src/puzzle/day02_puzzle.cpp
+++ src/puzzle/day02_puzzle.cpp
@@ -1,6 +1,31 @@
+#include <sstream>
 #include "puzzle/day02_puzzle.hpp"
+#include "util/string_utils.hpp"
 
 int Day02Puzzle::solvePartOne(std::string &puzzleInput) {
+    auto lines = StringUtils::splitOnNewline("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    for (const auto &line: lines) {
+        std::map<std::string, int> colorCounts;
+        std::istringstream iss(line.substr(line.find(':') + 1));
+
+        std::string group;
+        while (std::getline(iss, group, ';')) {
+            std::istringstream groupStream(group);
+            std::string pair;
+
+            while (std::getline(groupStream, pair, ',')) {
+                std::istringstream pairStream(pair);
+                int count;
+                std::string color;
+
+                pairStream >> count >> color;
+                colorCounts[color] += count;
+            }
+        }
+
+        // TODO: Finish this
+    }
+
     return 0;
 }
 
