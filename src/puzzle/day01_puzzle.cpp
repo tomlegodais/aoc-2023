f7ca03b10a1325ae210e57de22eff02006f401a9
Tom LeGodais <tomlegodais@gmail.com>
2023-12-11 15:54:45 -0500

    code cleanup on day01_puzzle.cpp

--- src/puzzle/day01_puzzle.cpp
+++ src/puzzle/day01_puzzle.cpp
@@ -3,7 +3,7 @@
 #include "util/string_utils.hpp"
 
 int Day01Puzzle::solvePartOne(std::string &puzzleInput) {
-    auto lines = StringUtils::splitOnNewline(puzzleInput);
+    const auto lines = StringUtils::splitOnNewline(puzzleInput);
     int sum = 0;
 
     for (const auto &line: lines) {
@@ -22,8 +22,8 @@ int Day01Puzzle::solvePartOne(std::string &puzzleInput) {
 }
 
 int Day01Puzzle::solvePartTwo(std::string &puzzleInput) {
-    auto lines = StringUtils::splitOnNewline(puzzleInput);
-    std::map<std::string, char> numberMapping = {
+    const auto lines = StringUtils::splitOnNewline(puzzleInput);
+    const std::map<std::string, char> numberMapping = {
             {"one",   1},
             {"two",   2},
             {"three", 3},
@@ -40,10 +40,9 @@ int Day01Puzzle::solvePartTwo(std::string &puzzleInput) {
         std::vector<std::pair<int, int>> indices;
         for (auto i = 0; i < line.length(); i++) {
             if (std::isalpha(line[i])) {
-                for (const auto &pair: numberMapping) {
-                    const std::string &word = pair.first;
-                    if (line.substr(i, word.length()) == word) {
-                        indices.emplace_back(i, pair.second);
+                for (const auto & [fst, snd]: numberMapping) {
+                    if (const std::string &word = fst; line.substr(i, word.length()) == word) {
+                        indices.emplace_back(i, snd);
                         break;
                     }
                 }
@@ -52,12 +51,12 @@ int Day01Puzzle::solvePartTwo(std::string &puzzleInput) {
             }
         }
 
-        std::sort(indices.begin(), indices.end(), [](const auto &lhs, const auto &rhs) {
+        std::ranges::sort(indices, [](const auto &lhs, const auto &rhs) {
             return lhs.first < rhs.first;
         });
 
-        int firstDigit = indices[0].second;
-        int lastDigit = indices[indices.size() - 1].second;
+        const int firstDigit = indices[0].second;
+        const int lastDigit = indices[indices.size() - 1].second;
 
         sum += firstDigit * 10 + lastDigit;
     }
