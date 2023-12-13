39123778ddbe4585e123c88e3909c2620d93d304
Tom LeGodais <tomlegodais@gmail.com>
2023-12-13 11:40:02 -0500

    add day03_puzzle.cpp base class

---
+++ include/puzzle/day03_puzzle.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "day_puzzle.hpp"
+#include "puzzle_registrar.hpp"
+
+class [[maybe_unused]] Day03Puzzle final : public DayPuzzle {
+public:
+    explicit Day03Puzzle(const PuzzleService &puzzle_service) : DayPuzzle(puzzle_service) {}
+
+    int solvePartOne(std::string &puzzle_input) override;
+
+    int solvePartTwo(std::string &puzzle_input) override;
+
+private:
+    [[maybe_unused]] static PuzzleRegistrar<3, Day03Puzzle> registrar_;
+};
---
+++ src/puzzle/day03_puzzle.cpp
@@ -0,0 +1,12 @@
+#include "puzzle/day03_puzzle.hpp"
+#include "util/string_utils.hpp"
+
+int Day03Puzzle::solvePartOne(std::string &puzzle_input) {
+    return 0;
+}
+
+int Day03Puzzle::solvePartTwo(std::string &puzzle_input) {
+    return 0;
+}
+
+[[maybe_unused]] PuzzleRegistrar<3, Day03Puzzle> Day03Puzzle::registrar_("Gear Ratios");
