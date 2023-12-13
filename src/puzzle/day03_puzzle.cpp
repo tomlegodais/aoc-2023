91f71a7b572f33944579e30c165c452993f8c793
Tom LeGodais <tomlegodais@gmail.com>
2023-12-13 18:01:40 -0500

    solve part one of day03_puzzle.cpp

--- src/puzzle/day03_puzzle.cpp
+++ src/puzzle/day03_puzzle.cpp
@@ -1,13 +1,47 @@
 #include "puzzle/day03_puzzle.hpp"
 #include "util/string_utils.hpp"
-
-
 #include <iostream>
 
-int Day03Puzzle::solvePartOne(std::string &puzzle_input) {
-    std::cout << "Hello World" << std::endl;
+bool isSymbol(const char c) {
+    return !isdigit(c) && c != '.';
+}
 
-    return 0;
+bool isAdjacent(const std::vector<std::string> &schematic, const int x, const int y) {
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            const int newX = x + dx;
+            if (const int newY = y + dy; newX >= 0 && newX < schematic.size() && newY >= 0 && newY < schematic[0].size() && isSymbol(schematic[newX][newY])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int Day03Puzzle::solvePartOne(std::string &puzzle_input) {
+    const std::vector<std::string> schematic = StringUtils::splitOnNewline(puzzle_input);
+    int totalSum = 0;
+
+    for (int i = 0; i < schematic.size(); i++) {
+        for (int j = 0; j < schematic[i].size(); j++) {
+            if ((j == 0 || !isdigit(schematic[i][j - 1])) && isdigit(schematic[i][j])) {
+                std::string numberStr;
+                int k = j;
+                bool adjacentToSymbol = false;
+                while (k < schematic[i].size() && isdigit(schematic[i][k])) {
+                    numberStr += schematic[i][k];
+                    if (isAdjacent(schematic, i, k)) adjacentToSymbol = true;
+                    k++;
+                }
+                if (adjacentToSymbol) {
+                    totalSum += std::stoi(numberStr);
+                }
+                j = k - 1;
+            }
+        }
+    }
+
+    return totalSum;
 }
 
 int Day03Puzzle::solvePartTwo(std::string &puzzle_input) {
