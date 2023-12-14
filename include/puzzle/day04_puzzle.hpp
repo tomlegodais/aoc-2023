9d65a395b2c766665b3ca180c0f86a874e1f0372
tomlegodais <tomlegodais@gmail.com>
2023-12-14 18:40:57 -0500

    add base for day04_puzzle.cpp

---
+++ include/puzzle/day04_puzzle.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "day_puzzle.hpp"
+#include "puzzle_registrar.hpp"
+
+class [[maybe_unused]] Day04Puzzle final : public DayPuzzle {
+public:
+    explicit Day04Puzzle(const PuzzleService &puzzle_service) : DayPuzzle(puzzle_service) {}
+
+    int solvePartOne(std::string &puzzle_input) override;
+
+    int solvePartTwo(std::string &puzzle_input) override;
+
+private:
+    [[maybe_unused]] static PuzzleRegistrar<4, Day04Puzzle> registrar_;
+};
---
+++ src/puzzle/day04_puzzle.cpp
@@ -0,0 +1,11 @@
+#include "puzzle/day04_puzzle.hpp"
+
+int Day04Puzzle::solvePartOne(std::string &puzzle_input) {
+    return 0;
+}
+
+int Day04Puzzle::solvePartTwo(std::string &puzzle_input) {
+    return 0;
+}
+
+[[maybe_unused]] PuzzleRegistrar<4, Day04Puzzle> Day04Puzzle::registrar_("Scratchcards");
