2b08a7399403d7eeaf97595d2f132b52df6c34cc
Tom LeGodais <tomlegodais@gmail.com>
2023-12-13 22:50:49 -0500

    solve part two of day03_puzzle.cpp

--- src/puzzle/day03_puzzle.cpp
+++ src/puzzle/day03_puzzle.cpp
@@ -1,6 +1,7 @@
 #include "puzzle/day03_puzzle.hpp"
 #include "util/string_utils.hpp"
 #include <iostream>
+#include <set>
 
 bool isSymbol(const char c) {
     return !isdigit(c) && c != '.';
@@ -44,8 +45,51 @@ int Day03Puzzle::solvePartOne(std::string &puzzle_input) {
     return totalSum;
 }
 
+int findPartNumber(const std::vector<std::string> &schematic, int x, int y) {
+    if (x >= 0 && x < schematic.size() && y >= 0 && y < schematic[0].size() && isdigit(schematic[x][y])) {
+        while (y > 0 && isdigit(schematic[x][y - 1])) {
+            y--;
+        }
+
+        std::string numberStr;
+        while (y < schematic[x].size() && isdigit(schematic[x][y])) {
+            numberStr += schematic[x][y];
+            y++;
+        }
+
+        return std::stoi(numberStr);
+    }
+
+    return -1;
+}
+
 int Day03Puzzle::solvePartTwo(std::string &puzzle_input) {
-    return 0;
+    const std::vector<std::string> schematic = StringUtils::splitOnNewline(puzzle_input);
+    int totalSum = 0;
+
+    for (int i = 0; i < schematic.size(); i++) {
+        for (int j = 0; j < schematic[i].size(); j++) {
+            if (schematic[i][j] == '*') {
+                std::set<int> adjacentNumbers;
+
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        if (dx == 0 && dy == 0) continue;
+
+                        if (int partNumber = findPartNumber(schematic, i + dx, j + dy); partNumber != -1)
+                            adjacentNumbers.insert(partNumber);
+                    }
+                }
+
+                if (adjacentNumbers.size() == 2) {
+                    auto it = adjacentNumbers.begin();
+                    totalSum += *it * *++it;
+                }
+            }
+        }
+    }
+
+    return totalSum;
 }
 
 [[maybe_unused]] PuzzleRegistrar<3, Day03Puzzle> Day03Puzzle::registrar_("Gear Ratios");
