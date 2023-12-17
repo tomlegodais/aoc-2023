e9f7097429e51911af4deed27092e6aa7ead4532
tomlegodais <tomlegodais@gmail.com>
2023-12-17 13:11:15 -0500

    test disabling specialization

--- include/puzzle/day_puzzle.hpp
+++ include/puzzle/day_puzzle.hpp
@@ -15,11 +15,11 @@ public:
     static const char *getTitle();
 };
 
-template<>
-int DayPuzzle<1>::solvePartOne(PuzzleService &, const std::vector<std::string> &);
-
-template<>
-int DayPuzzle<1>::solvePartTwo(PuzzleService &, const std::vector<std::string> &);
-
-template<>
-const char *DayPuzzle<1>::getTitle();
+// template<>
+// int DayPuzzle<1>::solvePartOne(PuzzleService &, const std::vector<std::string> &);
+//
+// template<>
+// int DayPuzzle<1>::solvePartTwo(PuzzleService &, const std::vector<std::string> &);
+//
+// template<>
+// const char *DayPuzzle<1>::getTitle();
