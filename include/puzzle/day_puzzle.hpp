d8eaee884e345b8469fa3b78bfda822c2b46d784
Tom LeGodais <tomlegodais@gmail.com>
2023-12-16 22:03:29 -0500

    test fixing build error

--- include/puzzle/day_puzzle.hpp
+++ include/puzzle/day_puzzle.hpp
@@ -14,3 +14,12 @@ public:
 
     static const char *getTitle();
 };
+
+template<>
+int DayPuzzle<1>::solvePartOne(PuzzleService &, const std::vector<std::string> &);
+
+template<>
+int DayPuzzle<1>::solvePartTwo(PuzzleService &, const std::vector<std::string> &);
+
+template<>
+const char *DayPuzzle<1>::getTitle();
